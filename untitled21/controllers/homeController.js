87e1490ca9966044bf94e7d1bbbd7363da9f6d31
Bailey June <baileyjune06@gmail.com>
2016-05-14 15:39:17 -0700

    added controller

--- untitled21/controllers/homeController.js
+++ untitled21/controllers/homeController.js
@@ -1,3 +1,10 @@
 /**
  * Created by Bailey on 5/14/2016.
  */
+var recycleApp = angular.module('recycleApp', []);
+
+recipeApp.controller('HomeCtrl', ['$scope', '$http',
+    function($scope, $rootScope, $http){
+
+
+    }]);
