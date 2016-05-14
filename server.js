7211344771a8cb37d94191a6e84d37478a4789ae
Bailey June <baileyjune06@gmail.com>
2016-05-14 15:01:54 -0700

    Removed manual server and dependencies

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-var mongoDBConnection = require('./db.recycling_app.config');
-console.log("DB connection at: " + mongoDBConnection.uri);
-mongoose.connect(mongoDBConnection.uri);
