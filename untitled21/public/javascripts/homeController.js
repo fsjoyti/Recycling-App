f3945cf6778484b0a85fbef7ab0f17367c6d320c
Bailey June <baileyjune06@gmail.com>
2016-05-15 13:08:08 -0700

    typo

--- untitled21/public/javascripts/homeController.js
+++ untitled21/public/javascripts/homeController.js
@@ -33,7 +33,7 @@ recycleApp.controller('InfoCtrl', ['$scope', '$http',
                     $scope.searched="true"
                 }
                 else {
-                    $scope.searchpass=!$scope.searchpass;
+                    $scope.searchPass="false";
                     $scope.searched="true";
                 }
             }, function errorCallback(response) {
