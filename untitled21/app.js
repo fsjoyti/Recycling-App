8b9e127789963977c729b44ada962f3b6eecd8fc
Fahmida Joyti <fsjoyti@iastate.edu>
2016-05-14 17:59:34 -0700

    File modified

--- untitled21/app.js
+++ untitled21/app.js
@@ -1,6 +1,7 @@
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,8 +10,8 @@ var routes = require('./routes/index');
 //var users = require('./routes/users');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017');
-var app = express();
+//mongoose.connect('mongodb://localhost:27017');
+
 var users = require('./routes/users');
 
 // view engine setup
@@ -23,41 +24,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+http.listen(3001,function () {
+  console.log("Example app listening at http://localhost:3001");
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
