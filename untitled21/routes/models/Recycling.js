0dba61b2541a745687d8ddc648231f7f59c4de79
Fahmida Joyti <fsjoyti@iastate.edu>
2016-05-15 15:37:02 -0700

    Cleaned up some code

--- untitled21/routes/models/Recycling.js
+++ untitled21/routes/models/Recycling.js
@@ -15,49 +15,7 @@ var ResinSchema = new Schema({
 });
 
 var Recycling = mongoose.model('Recycling', ResinSchema);
-/*
 
-
-
-var recycle3 = new Recycling(
-    {
-        RecycleId: 3,
-        number :  1,
-        description:'ddwd',
-        examples : ['dwdwd'],
-        location: [{
-            locationID:1,
-            zipcode :  98210,
-            name:'Blablawfef',
-            city : 'Seattle',
-            address : 'dddfd ave ne'
-
-        },
-            {
-                locationID:2,
-                zipcode :  98210,
-                name:'fefef',
-                city : 'Seattle',
-                address : 'fefefefe'
-
-            },
-            {
-                locationID:3,
-                zipcode :  98210,
-                name:'fefef',
-                city : 'Seattle',
-                address : 'fefefefe'
-
-            }
-        ]
-    }
-);
-recycle3.save (function(err){
-    if (err) console.log("Error on save!");
-    console.log('Saved!');
-
-});
-*/
 
 
 module.exports = mongoose.model('Recycling',ResinSchema);
